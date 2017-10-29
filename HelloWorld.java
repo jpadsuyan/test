eec3393a6c3170067b68f8542cbc8e865cd2df88
JPadsuyan <fake.seeker15@gmail.com>
2017-10-29 10:37:21 +0900

    Second Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 	      System.out.println("Hello, world from GIT!");
+		System.out.println("Changes after First commit!");
 	   }
     
 }
