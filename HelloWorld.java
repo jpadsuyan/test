6627bbc528bdf526be67bb8dc7d9669dea8a6b0d
JPadsuyan <fake.seeker15@gmail.com>
2017-10-29 12:01:27 +0900

    Third Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	      System.out.println("Hello, world from GIT!");
 		System.out.println("Changes after First commit!");
+		System.out.println("Changes after Pushing to remote!");
 	   }
     
 }
